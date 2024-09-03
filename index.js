3a0bc518dde66289ec4bc20502f559e3e978f6e5
Thabang Selowa <mohale20cobra@gmail.com>
2024-09-03 10:45:17 +0200

    Add a type method to check error and calculate velocity

--- index.js
+++ index.js
@@ -14,14 +14,15 @@ const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
 
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+calcNewVel = (vel, acc, time) => {
+if (typeof vel !== 'number' || typeof acc !== 'number' || typeof time !== 'number'){
+  throw error('')
 }
+// Calculate to return new vel devided by time in seconds
+  return vel + (acc*time / 3600)
+};
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
